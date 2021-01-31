37b750710ecc55b6cc65cdba4913fb55a2ab209b
skruud <skruud@gmail.com>
2021-01-31 23:18:27 +0100

    improve functionality

--- main_gui.py
+++ main_gui.py
@@ -10,6 +10,7 @@ from safe_counter import SafeCounter
 from image_compressor import ImageCompressor
 from MainWindow import Ui_MainWindow
 from PyQt5.QtCore import QThread, pyqtSignal
+from PyQt5 import QtGui
 
 
 
@@ -42,8 +43,9 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             print(source_path)
             image_loader = ImageLoader(source_path)
             self.images = image_loader.get_images()
-            filenames = [image.split('\\')[-1] for image in self.images]
-            self.listWidgetFiles.addItems( filenames )
+            self.filenames = [image.split('\\')[-1] for image in self.images]
+            self.listWidgetFiles.clear()
+            self.listWidgetFiles.addItems( self.filenames )
             
         except:
             print('Invalid directory')
@@ -53,8 +55,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.quality = self.spinBoxQuality.value()
         source_path = self.lineEditSource.text()
         destination_path = self.lineEditDestination.text()
-        print(source_path)
-
+        
         # Set default/output directory
         os.chdir(destination_path)
 
@@ -66,31 +67,44 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         if (number_of_threads > number_of_images):
             number_of_threads = number_of_images
         
+        main_thread = threading.Thread( target=self.main_routine, args=(number_of_threads, ) ).start()
+            
+
+    def update_progressbar(self, msg):
+        progress = int(int(msg)/len(self.images)*100)
+        self.progressBar.setProperty("value", progress )
+        print(msg, progress)
+        if (progress==100):
+            self.pushButtonCompress.setEnabled(True)
+        else:
+            self.pushButtonCompress.setEnabled(False)
+            self.listWidgetFiles.item(int(msg)).setBackground(QtGui.QColor('sea green'))
+            
+        
+    def main_routine(self, number_of_threads):    
         threads = []
         for _ in range(number_of_threads):
             threads.append( threading.Thread( target=self.thread_routine ) )
 
         for thread in threads:
             thread.start() 
-
-    def update_progressbar(self, msg):
-        self.progressBar.setProperty("value", int(msg) )
-        if (int(msg)==100):
-            self.pushButtonCompress.setEnabled(True)
-        else:
-            self.pushButtonCompress.setEnabled(False)
         
+        for thread in threads:
+            thread.join()
+
+        self._signal.emit( len(self.images) )
     
     def thread_routine(self):
         file_n = self.safe_counter.get()
         while (file_n != -1):
             ImageCompressor(self.images[file_n], quality=self.quality)
             print(len(self.images), file_n)
+            self._signal.emit( file_n )
             file_n = self.safe_counter.get()
-            self._signal.emit( file_n/len(self.images)*100 )
+            
             
         print(file_n)
-        self._signal.emit( 100 )
+        #self._signal.emit( 100 )
 
        
 app = QtWidgets.QApplication(sys.argv)
